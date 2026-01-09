b5af6aa989ed5b7ec139345f93104e03329c8310
Prashant Kumawat <prashant253064@iiitmanipur.ac.in>
2026-01-09 19:46:38 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main (){
+
+    int n,i,flag;
+     flag = 0;
+
+    printf("enter the value of n : ");
+    scanf("%d", &n);
+
+   for( i=2; i<=n/2; i++){
+    if (n%i == 0){
+        flag = 1;
+        break;
+    }
+}
+    if (flag == 0){
+    printf("the number is a prime");
+}
+    else {
+    printf("the number is not  a prime");
+}
+  
+    
+    return 0; 
+}
